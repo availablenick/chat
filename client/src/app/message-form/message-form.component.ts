5c98b68cefd85f2c1c060b1f243064959b61663b
availablenick <leoap711@gmail.com>
2022-11-15 19:57:04 -0300

    feat: substitute input for textarea

--- client/src/app/message-form/message-form.component.ts
+++ client/src/app/message-form/message-form.component.ts
@@ -12,7 +12,9 @@ export class MessageFormComponent {
 
   onSubmit(event: Event): void {
     event.preventDefault();
-    const content = (<any>event.target).querySelector("textarea[name=content]").value;
+    const input = (<any>event.target).querySelector("input[name=content]");
+    const content = input.value;
+    input.value = "";
     this.http
       .post("http://localhost:5000/api/v1/messages", { content }, { withCredentials: true })
       .subscribe(() => { });
